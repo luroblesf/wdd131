019a654900a5390c0523daed64d37ce485e85126
luroblesf <iroblesfuentes@byupathway.edu>
2025-07-05 09:57:49 -0600

    assignment1

---
+++ scripts/getdates.js
@@ -0,0 +1,18 @@
+/* Current Year */
+
+function getCopyrightYear() {
+    const year = new Date().getFullYear();
+    return `&copy; ${year}`;
+}
+document.getElementById("currentYear").innerHTML = getCopyrightYear();
+
+/* Last Modified */
+
+function getLastModified() {
+    const lastModified = new Date(document.lastModified);
+    return `Last Modified: ${lastModified}`;
+}
+document.getElementById("lastModified").innerHTML = getLastModified();
+
+function updatePageVisitsCounter() {
+}
