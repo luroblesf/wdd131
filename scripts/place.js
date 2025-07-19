901a6cd9fd60a3116af0df99b3f87e91e184ef7a
luroblesf <iroblesfuentes@byupathway.edu>
2025-07-19 09:00:55 -0600

    w03

--- scripts/place.js
+++ scripts/place.js
@@ -1,19 +1,12 @@
-function getCopyrightYear() {
-    const year = new Date().getFullYear();
-    return `&copy; ${year}`;
+function getCurrentYear() {
+    return new Date().getFullYear();
 }
-document.getElementById("currentYear").innerHTML = getCopyrightYear();
-
-/* Last Modified */
-
-function getLastModified() {
-    const lastModified = new Date(document.lastModified);
-    return `Last Modified: ${lastModified}`;
-}
-document.getElementById("lastModified").innerHTML = getLastModified();
-
-function updatePageVisitsCounter() {
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var yearSpan = document.querySelector("#year span.highlight");
+    if (yearSpan) {
+        yearSpan.textContent = getCurrentYear();
+    }
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     var lastModifiedDate = document.lastModified;
@@ -23,17 +16,22 @@ document.addEventListener("DOMContentLoaded", function () {
         modifiedParagraph.textContent = "Last modified: " + lastModifiedDate;
     }
 });
+
 var temperatureCelsiusValue = 10;
 var temperatureCelsiusElement = document.getElementById("temperature");
 temperatureCelsiusElement.textContent = temperatureCelsiusValue + "°C";
 var windSpeedKmhValue = 5;
 var windSpeedKmhElement = document.getElementById("wind");
 windSpeedKmhElement.textContent = windSpeedKmhValue + " km/h";
+
 function calculateWindchill(temperatureCelsius, windSpeedKmh) {
     return 13.12 + 0.6215 * temperatureCelsius - 11.37 * Math.pow(windSpeedKmh, 0.16) + 0.3965 * temperatureCelsius * Math.pow(windSpeedKmh, 0.16);
 }
+
 var temperatureCelsius = 10;
 var windSpeedKmh = 5;
+
 var windChill = calculateWindchill(temperatureCelsius, windSpeedKmh);
+
 var windchillElement = document.getElementById("windChill");
 windchillElement.textContent = windChill.toFixed(1) + "°C";
